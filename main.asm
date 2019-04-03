576aa4cfa5e376c7cfee9d10d42e9ffeec44fae8
Harry Kha <38999102+khakuzz@users.noreply.github.com>
2019-04-03 16:19:51 +1100

    Part C Almost completed
    Just need to implement floor number 10 into the code for it to fully function normally.

--- main.asm
+++ main.asm
@@ -1,3 +1,4 @@
+
 .include "m2560def.inc"
 
 	.def row = r16 ; current row number
@@ -11,6 +12,26 @@
 	.equ INITROWMASK = 0x01 ; scan from the top row
 	.equ ROWMASK = 0x0F ; for obtaining input from Port D
 
+
+.macro do_lcd_command
+	ldi r16, @0
+	rcall lcd_command
+	rcall lcd_wait
+.endmacro
+.macro do_lcd_data
+	ldi r16, @0
+	rcall lcd_data
+	rcall lcd_wait
+.endmacro
+.macro do_lcd_data1
+	mov r16, @0
+	rcall lcd_data
+	rcall lcd_wait
+.endmacro
+
+.org 0
+	jmp RESET
+
 RESET:
 	ldi temp1, low(RAMEND) ; initialize the stack
 	out SPL, temp1
@@ -21,6 +42,15 @@ RESET:
 	ser temp1 ; PORTC is output
 	out DDRC, temp1
 	out PORTC, temp1
+
+
+	ser r16
+	out DDRF, r16
+	out DDRA, r16
+	clr r16
+	out PORTF, r16
+	out PORTA, r16
+
 main:
 	ldi cmask, INITCOLMASK ; initial column mask
 	clr col ; initial column
@@ -81,5 +111,153 @@ star:
 zero:
 	ldi temp1, 0 ; Set to zero
 convert_end:
+	ldi r17, 1
+	ldi r16, 1
+	push r16
+	clr r16
+	out DDRG, r16
+	pop r16
+	mov r18, temp1
+	push r18
+	cpi r18, 9
+		breq floor9
+		brge floor10
+	rjmp leftshift
+floor10:
+	push temp1
+	ser temp1
+	out DDRG, temp1 ;set Port G
+	pop temp1
+	ldi r18, 3
+	out PORTG, r18
+	rjmp leftshift
+floor9:
+	push temp1
+	ser temp1
+	out DDRG, temp1 ;set Port G
+	pop temp1
+	ldi r18, 1
+	out PORTG, r18
+leftshift:
+	cp r17, temp1
+		breq end
+	lsl r16
+	subi r16, -1
+	inc r17
+	rjmp leftshift
+end:
+	mov temp1, r16
 	out PORTC, temp1 ; Write value to PORTC
+
+	do_lcd_command 0b00111000 ; 2x5x7
+	rcall sleep_5ms
+	do_lcd_command 0b00111000 ; 2x5x7
+	rcall sleep_1ms
+	do_lcd_command 0b00111000 ; 2x5x7
+	do_lcd_command 0b00111000 ; 2x5x7
+	do_lcd_command 0b00001000 ; display off?
+	do_lcd_command 0b00000001 ; clear display
+	do_lcd_command 0b00000110 ; increment, no display shift
+	do_lcd_command 0b00001110 ; Cursor on, bar, no blink
+
+	do_lcd_data 'F'
+	do_lcd_data 'l'
+	do_lcd_data 'o'
+	do_lcd_data 'o'
+	do_lcd_data 'r'
+	do_lcd_data ' '
+	do_lcd_data 'r'
+	do_lcd_data 'e'
+	do_lcd_data 'q'
+	do_lcd_data 'u'
+	do_lcd_data 'e'
+	do_lcd_data 's'
+	do_lcd_data 't'
+	do_lcd_data ' '
+	pop r18
+	subi r18, -48
+	do_lcd_data1 r18
+
 	jmp main ; Restart main loop
+halt:
+	rjmp halt
+
+.equ LCD_RS = 7
+.equ LCD_E = 6
+.equ LCD_RW = 5
+.equ LCD_BE = 4
+
+.macro lcd_set
+	sbi PORTA, @0
+.endmacro
+.macro lcd_clr
+	cbi PORTA, @0
+.endmacro
+
+;
+; Send a command to the LCD (r16)
+;
+
+lcd_command:
+	out PORTF, r16
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_E
+	rcall sleep_1ms
+	ret
+
+lcd_data:
+	out PORTF, r16
+	lcd_set LCD_RS
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_E
+	rcall sleep_1ms
+	lcd_clr LCD_RS
+	ret
+
+lcd_wait:
+	push r16
+	clr r16
+	out DDRF, r16
+	out PORTF, r16
+	lcd_set LCD_RW
+lcd_wait_loop:
+	rcall sleep_1ms
+	lcd_set LCD_E
+	rcall sleep_1ms
+	in r16, PINF
+	lcd_clr LCD_E
+	sbrc r16, 7
+	rjmp lcd_wait_loop
+	lcd_clr LCD_RW
+	ser r16
+	out DDRF, r16
+	pop r16
+	ret
+
+.equ F_CPU = 16000000
+.equ DELAY_1MS = F_CPU / 4 / 1000 - 4
+; 4 cycles per iteration - setup/call-return overhead
+
+sleep_1ms:
+	push r24
+	push r25
+	ldi r25, high(DELAY_1MS)
+	ldi r24, low(DELAY_1MS)
+delayloop_1ms:
+	sbiw r25:r24, 1
+	brne delayloop_1ms
+	pop r25
+	pop r24
+	ret
+
+sleep_5ms:
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	ret
